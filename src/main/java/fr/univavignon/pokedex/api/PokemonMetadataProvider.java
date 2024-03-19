dd7c03563862e2af29de6774a5f7251ad202b218
AbdelouahedKANOUNI <https://github.com/CERI-L3-20222023-ApplicationsMobiles/tp2-gr3-5-6-abdelouahedKanouni.git>
2024-03-19 17:56:04 +0100

    TP4 (Épisode 2, Épisode 3) v5

--- src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
@@ -4,24 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PokemonMetadataProvider implements IPokemonMetadataProvider{
-    public final List<PokemonMetadata> pokemonMetadataList;
 
-    public PokemonMetadataProvider () {
-        pokemonMetadataList = new ArrayList<>();
-        PokemonMetadata bulbizarre = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
-        PokemonMetadata aquali = new PokemonMetadata(133, "Aquali", 186, 168, 260);
-        pokemonMetadataList.add(bulbizarre);
-        pokemonMetadataList.add(aquali);
+    List<PokemonMetadata> pokemonsMetadata;
+
+    public PokemonMetadataProvider() {
+        pokemonsMetadata = new ArrayList<>();
+        pokemonsMetadata.add(new PokemonMetadata(0, "Bulbasaur", 126, 126,
+                90));
+        //pickachu
+        pokemonsMetadata.add(new PokemonMetadata(25, "Pikachu", 112, 101,
+                70));
+
     }
-
     @Override
     public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-
-        PokemonMetadata results = null;
-        if(index < 0 || index > 150) throw new PokedexException("Invalid given index");
-        for (PokemonMetadata pokemonMetadata : pokemonMetadataList){
-            if(pokemonMetadata.getIndex() == index) results =pokemonMetadata ;
+        if(index < 0 || index >= 151)
+        {
+            throw new PokedexException("Invalid index");
         }
-        return results;
+        else
+        {
+            for (PokemonMetadata pokemonMetadata : pokemonsMetadata) {
+                if (pokemonMetadata.getIndex() == index) {
+                    return pokemonMetadata;
+                }
+            }
+        }
+        throw new PokedexException("PokemonMetaData not found");
     }
 }
